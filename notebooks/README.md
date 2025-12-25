eabd12aeb614b2c917c70c6f6e194591592591ff
Rudra Prasad Bhuyan <rudraprasadbhuyan999@gmail.com>
2025-12-25 20:17:20 +0530

    Fill NULL Values

--- notebooks/README.md
+++ notebooks/README.md
@@ -16,14 +16,16 @@ Starting og the notebook indicate the family they belonging
 
 ## Data Cleaning Notebooks and Finding
 1. **Check Null values** 
-   - [Notebook Links](../notebooks/data-cleaning/c5-c5-check-null-values.ipynb)
-    - `next_month_bought` cols is most blank
-    - Some prices are missing, MRP, Offer 
+   - [Notebook Links](../notebooks/data-cleaning/c5-check-null-values.ipynb)
+   - `next_month_bought` cols is most blank
+   - Some prices are missing, MRP, Offer 
     - 
-
+2. Fill the null values
+   - [Notebook Links](../notebooks/data-cleaning/c6-fill-the-null-values.ipynb)
+   - 
 ---
 
 **Useful Websites**
 
-- https://html.onlineviewer.net/
-- https://codebeautify.org/htmlviewer/
+- HTML code viewer: https://codebeautify.org/htmlviewer/
+- Regular Expression tool: https://regex101.com/
--- script/fill_null_values.py
+++ script/fill_null_values.py
@@ -5,23 +5,39 @@
 # 3. Inside the for loop
 # 4. check the null values
 # 5. Using polars library
+# 6. Fill the null values 
+# 7. Strategy with the forward, backward, other filling methods
 # ----------------------------------------------------------------
 
+import os
 import polars as pl
 from loguru import logger
 
+os.makedirs("logs", exist_ok=True)
 logger.remove()
 logger.add(
-    "null_value_log_{time}.log",
+    "logs/fill_null_values_log_{time}.log",
     format="{time: YYYY-MM-DD HH:mm:ss} | {level} | {message}"
 )
 
+def forward_filling(col):
+    return dl.with_columns(
+        pl.col(col).fill_null(strategy='forward')
+    )
+
+    
 try: 
     for i in range(1, 11):
         path = fr"C:\Users\Rudra\Desktop\laptop-price-predictor\data\amazon_laptop_{i}.csv"
         logger.info(f"\n\n\n {path}")
         
         dl = pl.read_csv(path)
+
+        forward_fill_cols = ['rating', 'total_rating', 'next_month_bought', 'offer', 'special_features']
+        dl = forward_filling(forward_fill_cols)
+        logger.info(f""""Forward Fill done with 
+                        ['rating', 'total_rating', 'next_month_bought', 'offer', 'special_features']
+                    """)
         
         for col in dl.columns:
             logger.info(f"{col}: {dl[col].null_count()}")
